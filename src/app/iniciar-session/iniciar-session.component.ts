11b7bcee6ff866f2be653605feefd0f456a78a95
Atixar <gabo_cai_1990@hotmail.com>
2023-01-10 21:20:52 -0300

    iniciar sesion ts completo

--- src/app/iniciar-session/iniciar-session.component.ts
+++ src/app/iniciar-session/iniciar-session.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AutenticacionService } from '../servicios/autenticacion.service';
 @Component({
   selector: 'app-iniciar-session',
   templateUrl: './iniciar-session.component.html',
@@ -8,7 +10,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class IniciarSessionComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private autenticacionService: AutenticacionService,
+    private ruta: Router
+  ) {
     this.form = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(3)]],
@@ -21,5 +27,16 @@ export class IniciarSessionComponent implements OnInit {
   }
   get Password() {
     return this.form.get('password');
+  }
+
+  onEnviar(event: Event) {
+    event.preventDefault;
+    this.autenticacionService
+      .IniciarSesion(this.form.value)
+      .subscribe((data) => {
+        console.log('DATA:' + JSON.stringify(data));
+
+        this.ruta.navigate(['/portfolio']);
+      });
   }
 }
